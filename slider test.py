6fddf6804914a876dae3ec30d4abc40ccea0be74
Bob Thacker <38263269+MrT-CompSci@users.noreply.github.com>
2018-04-12 08:59:23 +0100

    Title added
    w

--- slider test.py
+++ slider test.py
@@ -22,7 +22,7 @@ def color_change():
     app.bg = (int(slider_r.value), int(slider_g.value), int(slider_b.value))
     textbox_hex.value = "#{0:02x}{1:02x}{2:02x}".format(clamp(slider_r.value), clamp(slider_g.value), clamp(slider_b.value))
 
-app = App()
+app = App(title = "Colour Mixer", height = 300, width = 400)
 
 #text = Text(app, text="Colour Mixer")
 
